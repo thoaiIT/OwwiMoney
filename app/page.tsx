a1ed6fa3339d36fa893569676bcd77df9bb112f1
Thoai1012 <nvthoai2k1@gmail.com>
2023-11-06 15:01:43 +0700

    init

---
+++ app/page.tsx
@@ -0,0 +1,5 @@
+import { Button } from '@radix-ui/themes';
+
+export default function Home() {
+  return <Button color="tomato"> radix colorz </Button>;
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config, import { radixThemePreset } from 'radix-themes-tw';} */
+
+module.exports = {
+  content: ['./app/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
