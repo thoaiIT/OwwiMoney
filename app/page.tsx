8e7ae9be1b920b3274ed0484066532f5d4d981f0
huynqq <ibuhuy105@gmail.com>
2023-11-13 09:46:51 +0700

    Add CommonButton component and @radix-ui/react-slot dependency, update package.json with new dependencies

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { CommonButton } from '../components/button';
 import CommonCard from '../ui/components/CommonCard';
 import ThemeSwitch from '../ui/components/theme-switch';
 import Logo from '../ui/Logo';
@@ -10,6 +11,12 @@ export default function Home() {
         <Logo />
         <div>hello world!!</div>
         <div>hello world!!</div>
+        <CommonButton
+          size={'default'}
+          variant={'outline'}
+        >
+          adas
+        </CommonButton>
       </CommonCard>
     </div>
   );
