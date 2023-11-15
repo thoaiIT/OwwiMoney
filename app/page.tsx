7470ebf04bc6e8bc6736140252d5b20dc14b3801
hduytins <hduytins@gmail.com>
2023-11-15 10:57:37 +0700

    feat: implement success progress

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import CommonInput from '../components/input';
 import CommonCard from '../ui/components/CommonCard';
 import ThemeSwitch from '../ui/components/theme-switch';
 import Logo from '../ui/Logo';
+import Success from '../components/success';
 
 export default function Home() {
   return (
@@ -31,6 +32,7 @@ export default function Home() {
           intent="primary"
         />
       </Flex>
+      <Success />
     </div>
   );
 }
