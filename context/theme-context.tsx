24ee4961cf4630c242f9d56779917ee77e65403b
Thoai1012 <nvthoai2k1@gmail.com>
2023-11-16 13:35:51 +0700

    save theme

--- context/theme-context.tsx
+++ context/theme-context.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { useState, createContext, useContext } from 'react';
+import { createContext, useContext, useEffect, useState } from 'react';
 
 type Theme = 'light' | 'dark';
 
@@ -20,12 +20,29 @@ export const ThemeContextProvider = ({ children }: ThemeContextProviderProps) =>
   const toggleTheme = () => {
     if (theme === 'light') {
       setTheme('dark');
+      window.localStorage.setItem('theme', 'dark');
       document.documentElement.classList.add('dark');
     } else {
       setTheme('light');
+      window.localStorage.setItem('theme', 'light');
       document.documentElement.classList.remove('dark');
     }
   };
+
+  useEffect(() => {
+    const localTheme = window.localStorage.getItem('theme') as Theme | null;
+
+    if (localTheme) {
+      setTheme(localTheme);
+
+      if (localTheme === 'dark') {
+        document.documentElement.classList.add('dark');
+      }
+    } else if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      setTheme('dark');
+      document.documentElement.classList.add('dark');
+    }
+  }, []);
 
   return (
     <ThemeContext.Provider
