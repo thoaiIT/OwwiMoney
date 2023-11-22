9464d6d4c41fffbe0e3c106d68a1f26d4ab8814e
Thoai1012 <nvthoai2k1@gmail.com>
2023-11-22 16:28:51 +0700

    add title

--- app/(dashboard)/transactions/TabClient.tsx
+++ app/(dashboard)/transactions/TabClient.tsx
@@ -1,7 +1,9 @@
 'use client';
 import { CommonTabs, TabsContent, TabsList, type tabsListType } from '@/components/Tab';
+import { CommonButton } from '@/components/button';
 import type React from 'react';
 import type { ReactNode } from 'react';
+import { FaPlus } from 'react-icons/fa';
 
 type TabClientProps = {
   defaultValue: string;
@@ -12,11 +14,18 @@ type TabClientProps = {
 const TabClient: React.FC<TabClientProps> = ({ defaultValue, tabNames, tabContents }) => {
   return (
     <CommonTabs defaultValue={defaultValue}>
-      <TabsList tabNames={tabNames} />
+      <div className="flex justify-between">
+        <TabsList tabNames={tabNames} />
+        <CommonButton className="w-[208px] duration-300 transition-all bg-theme-component flex gap-2 hover:duration-300 hover:transition-all hover:bg-theme-component hover:opacity-80 hover:ring-0">
+          <FaPlus />
+          Add Transactions
+        </CommonButton>
+      </div>
       {tabContents.map((tabContent) => (
         <TabsContent
           key={tabContent.value}
           value={tabContent.value}
+          className="mt-6"
         >
           {tabContent.children}
         </TabsContent>
