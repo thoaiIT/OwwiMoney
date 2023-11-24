fe552456925778262e4c0f8d5bfa844e4bcc6a46
huynqq <ibuhuy105@gmail.com>
2023-11-24 16:29:28 +0700

    Refactor NavBar component and import it in CoreDashboard

--- components/dashboard/index.tsx
+++ components/dashboard/index.tsx
@@ -1,11 +1,9 @@
 'use client';
 
-import Breadcrumb from '@/components/breadscrumb';
+import NavBar from '@/components/dashboard/NavBar';
 import SideBar from '@/components/dashboard/SideBar';
 import Loading from '@/components/loading';
 import { ScrollCustom } from '@/components/scroll';
-import ThemeSwitch from '@/components/theme-switch';
-import { Box } from '@radix-ui/themes';
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
 import React, { Suspense, useEffect } from 'react';
@@ -29,13 +27,7 @@ const CoreDashboard = ({ children }: { children: React.ReactNode }) => {
         <SideBar />
         <ScrollCustom className="h-full w-full lg:w-5/6 md:w-3/5 lg:px-8 lg:py-4 xl:px-12 md:p-6 flex flex-col">
           <Suspense fallback={<Loading />}>
-            <div className="flex justify-between h-20 items-center">
-              <Breadcrumb />
-              <div>
-                <ThemeSwitch />
-                <Box>hello</Box>
-              </div>
-            </div>
+            <NavBar />
           </Suspense>
           {children}
         </ScrollCustom>
